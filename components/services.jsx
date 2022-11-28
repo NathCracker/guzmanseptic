045757a8d39157f6138e32b94950798475e3975b
Nath Dabne <nathaniel.v.faa@gmail.com>
2022-11-28 23:04:04 +0800

    Cards Animation

--- components/services.jsx
+++ components/services.jsx
@@ -10,22 +10,26 @@ const services = () => {
   const cardAnim = useRef(null);
   useEffect(() => {
     const el = h1Text.current;
-    const el2 = cardAnim.current;
+    const cards = gsap.utils.toArray('.cards');
+    cards.forEach(card => {
+      gsap.fromTo(card, {y: 100, opacity:0}, {y:0, opacity: 1, duration: 0.5,
+        scrollTrigger:{
+          trigger: card,
+        }
+        })
+      });
     gsap.fromTo(el, {x: -50, opacity: 0}, {x: 0, opacity: 1, duration: 1, scrollTrigger:{
       trigger: el
     }});
-    gsap.fromTo(el2, {y: 100, opacity: 0}, {y: 0, opacity: 1, delay: 0.5, duration: 1, scrollTrigger:{
-      trigger: el2
-    }} )
 
   }, []);
 
   return (
     <div className='bg-gradient-to-bl from-gray-100 to-white font-custom text-center w-full max-h-full flex flex-col items-center' id='services'>
         <h1 className='text-5xl pt-10 font-lobster' ref={h1Text}>Our Services</h1>
-        <div ref={cardAnim} className='flex justify-around items-center gap-5 py-10 max-sm:flex-wrap max-sm:gap-5 max-md:flex-wrap lg:flex-wrap max-lg:flex-wrap w-8/12'>
+        <div className='flex justify-around items-center gap-5 py-10 max-sm:flex-wrap max-sm:gap-5 max-md:flex-wrap lg:flex-wrap max-lg:flex-wrap w-8/12'>
             {offertable.map((table) => (
-                <div className='bg-white justify-start w-60 p-3 rounded-xl transition-all hover:-translate-y-2 duration-300 shadow-xl hover:shadow-2xl max-sm:w-11/12 max-md:w-2/6 max-md:m-5 max-lg:w-2/6 max-lg:m-5 lg:m-5 '>
+                <div className='cards bg-white justify-start w-60 p-3 rounded-xl transition-all hover:-translate-y-2 duration-300 shadow-xl hover:shadow-2xl max-sm:w-11/12 max-md:w-2/6 max-md:m-5 max-lg:w-2/6 max-lg:m-5 lg:m-5 '>
                 <img className='w-full h-52 m-0 p-1 object-cover rounded-xl' src={table.url} alt="" />
                 <div className='flex justify-end flex-col items-start'>
                     <h1 className='text-xl font-bold py-1'>{table.title}</h1>
